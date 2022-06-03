11d67143e6a2785da0f261a92640c6fd24fb13f6
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-06-03 12:28:24 +0200

    api fully fetched

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,7 @@
 // 9.993682 // long
 
 fetch('https://api.openweathermap.org/data/2.5/weather?lat=53.551086&lon=9.993682&appid=6b2723a1c78fa552dac0f78569b46380')
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+    });
